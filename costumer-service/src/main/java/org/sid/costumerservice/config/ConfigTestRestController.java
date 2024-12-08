0cebb1d931431d1b04475631d9be426132f9b678
nouhaila abdtouirsi <95345790+NouhailaAbdtouirsi@users.noreply.github.com>
2024-12-08 22:06:17 +0100

    Finalizing config repo

--- costumer-service/src/main/java/org/sid/costumerservice/config/ConfigTestRestController.java
+++ costumer-service/src/main/java/org/sid/costumerservice/config/ConfigTestRestController.java
@@ -10,9 +10,16 @@ import java.util.Map;
 @RestController
 @RefreshScope
 public class ConfigTestRestController {
+    @Value("${global.params.p1}")
+    private String p1;
+    @Value("${global.params.p2}")
+    private String p2;
     @Autowired
     private CustomerConfigParams customerConfigParams;
-
+    @GetMapping("/testConfig1")
+    public Map<String,String> configTest(){
+        return Map.of("p1",p1,"p2",p2);
+    }
     @GetMapping("/testConfig2")
     public CustomerConfigParams configTest2(){
         return customerConfigParams;
--- costumer-service/src/main/java/org/sid/costumerservice/config/CustomerConfigParams.java
+++ costumer-service/src/main/java/org/sid/costumerservice/config/CustomerConfigParams.java
@@ -3,6 +3,6 @@ package org.sid.costumerservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "costumer.params")
+@ConfigurationProperties(prefix = "costumer.param")
 public record CustomerConfigParams(int x, int y) {
 }
